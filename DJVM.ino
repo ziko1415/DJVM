0d434c8877ec0a25a34cbdba75383eb7ae56f853
beitak.tech <122798915+ziko1415@users.noreply.github.com>
2024-08-16 22:37:29 +0300

    Update DJVM.ino

--- DJVM.ino
+++ DJVM.ino
@@ -1,6 +1,6 @@
 // You can have from 1-6 sliders 
 const int NUM_SLIDERS = 6;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4, A6};
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4, A5};
 
 int analogSliderValues[NUM_SLIDERS];
 
